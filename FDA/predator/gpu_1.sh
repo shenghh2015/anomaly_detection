13e70ff90524b55b880c2581b5a7f6a02ec40e1d
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-05-06 21:09:10 -0500

    v100

--- FDA/predator/gpu_1.sh
+++ FDA/predator/gpu_1.sh
@@ -1,1 +1,1 @@
-python AE.py --gpu 0 --cn 6 --fr 32 --ks 5 --bn True --lr 1e-4 --step 100000 --bz 200 --dataset total --train 85000
+python AE.py --gpu 1 --cn 6 --fr 32 --ks 5 --bn True --lr 1e-4 --step 100000 --bz 200 --dataset total --train 85000
