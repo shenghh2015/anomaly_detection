c28fbd0dba4cddf7ec5d1c8a8d973e36f67a864e
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-05-07 15:42:21 -0500

    v100

--- MRI/load_data.py
+++ MRI/load_data.py
@@ -9,7 +9,8 @@ def load_MRI_true_data(docker = False, train = 65000, val = 600, normal = 1000, 
 	else:
 		dataset_folder = '/shared/planck/CommonData/MRI/anomaly_detection_data'
 	img = np.load(os.path.join(dataset_folder, 'axial_batch2_256x256.npy'))
+	img_test = img[len(img)-1000:,:]
 	img_MP =  np.load(os.path.join(dataset_folder, 'axial_batch2_256x256_artifact_1000.npy'))
-	X_trn, X_val, X_n, X_a = img[:train,:], img[65000:65000+val,:], img[65600:65600+normal,:], img_MP[:anomaly,:]
+	X_trn, X_val, X_n, X_a = img[:train,:], img[65000:65000+val,:], img_test[:normal,:], img_MP[:anomaly,:]
 	
 	return X_trn, X_val, X_n, X_a
