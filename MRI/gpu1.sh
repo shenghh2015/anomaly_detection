f1df90f3510099b1106f1c0fb444cc0dfbf813ad
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-05-07 16:21:11 -0500

    v100

--- MRI/gpu1.sh
+++ MRI/gpu1.sh
@@ -1,1 +1,1 @@
-python AE.py --gpu 1 --cn 2 --fr 32 --ks 5 --bn True --lr 1e-5 --step 100000 --bz 50 --train 65000
+python AE.py --gpu 1 --cn 4 --fr 32 --ks 5 --bn True --lr 1e-5 --step 100000 --bz 50 --train 65000
