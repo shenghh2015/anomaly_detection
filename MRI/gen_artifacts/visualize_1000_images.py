baf9435cfb9bd36ea937e95dee2dab3e0b627304
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-05-08 03:12:34 -0500

    v100

--- MRI/gen_artifacts/visualize_1000_images.py
+++ MRI/gen_artifacts/visualize_1000_images.py
@@ -7,19 +7,19 @@ f_MPs = np.load(dataset_folder+'/axial_batch2_256x256_artifact.npy')
 def save_recon_images(img_file_name, imgs, recons, fig_size):
 	from matplotlib.backends.backend_agg import FigureCanvasAgg
 	from matplotlib.figure import Figure
-	imgs, recons, errs = np.squeeze(imgs), np.squeeze(recons), np.squeeze(errs)
+	imgs, recons = np.squeeze(imgs), np.squeeze(recons)
 	test_size = imgs.shape[0]
 	indxs = np.random.randint(0,int(test_size),3)
 # 	fig_size = (8,6)
 	fig_size = fig_size
 	fig = Figure(figsize=fig_size)
 	rows, cols = 2, 3
-	ax = fig.add_subplot(rows, cols, 1); cax=ax.imshow(imgs[indxs[0],:],cmap='gray'); fig.colorbar(cax); ax.set_title('Image-{}'.format(idxs[0])); ax.set_ylabel('f') 
-	ax = fig.add_subplot(rows, cols, 2); cax=ax.imshow(imgs[indxs[1],:],cmap='gray'); fig.colorbar(cax); ax.set_title('Image-{}'.format(idxs[1]));
-	ax = fig.add_subplot(rows, cols, 3); cax=ax.imshow(imgs[indxs[2],:],cmap='gray'); fig.colorbar(cax); ax.set_title('Image-{}'.format(idxs[2]));
-	ax = fig.add_subplot(rows, cols, 4); cax=ax.imshow(recons[indxs[1],:],cmap='gray'); fig.colorbar(cax); ax.set_ylabel('f_MP')
-	ax = fig.add_subplot(rows, cols, 5); cax=ax.imshow(recons[indxs[2],:],cmap='gray'); fig.colorbar(cax);
-	ax = fig.add_subplot(rows, cols, 6); cax=ax.imshow(recons[indxs[3],:],cmap='gray'); fig.colorbar(cax);
+	ax = fig.add_subplot(rows, cols, 1); cax=ax.imshow(imgs[indxs[0],:],cmap='gray'); fig.colorbar(cax); ax.set_title('Image-{}'.format(indxs[0])); ax.set_ylabel('f') 
+	ax = fig.add_subplot(rows, cols, 2); cax=ax.imshow(imgs[indxs[1],:],cmap='gray'); fig.colorbar(cax); ax.set_title('Image-{}'.format(indxs[1]));
+	ax = fig.add_subplot(rows, cols, 3); cax=ax.imshow(imgs[indxs[2],:],cmap='gray'); fig.colorbar(cax); ax.set_title('Image-{}'.format(indxs[2]));
+	ax = fig.add_subplot(rows, cols, 4); cax=ax.imshow(recons[indxs[0],:],cmap='gray'); fig.colorbar(cax); ax.set_ylabel('f_MP')
+	ax = fig.add_subplot(rows, cols, 5); cax=ax.imshow(recons[indxs[1],:],cmap='gray'); fig.colorbar(cax);
+	ax = fig.add_subplot(rows, cols, 6); cax=ax.imshow(recons[indxs[2],:],cmap='gray'); fig.colorbar(cax);
 	canvas = FigureCanvasAgg(fig)
 	canvas.print_figure(img_file_name, dpi=100)
 
@@ -27,7 +27,7 @@ result_folder = dataset_folder+'/images'
 if not os.path.exists(result_folder):
 	os.system('mkdir -p {}'.format(result_folder))
 
-for i in range(2):
+for i in range(1000):
 	img_file_name = result_folder+'/group_{}'.format(i)
 	save_recon_images(img_file_name, images, f_MPs, fig_size = [11,5])
 
