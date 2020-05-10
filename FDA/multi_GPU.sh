59ac3b5a738f1d3b6dca9745c8776f47ce23f48d
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-05-10 01:48:15 -0500

    v100

--- FDA/multi_GPU.sh
+++ FDA/multi_GPU.sh
@@ -1,25 +1,5 @@
-# ### May 8, 2020
-# JOB: python AE.py --gpu 0 --cn 4 --fr 32 --ks 5 --bn True --lr 5e-6 --step 100000 --bz 50 --train 65000 --val 400 --test 400 --noise 0
-# JOB: python AE.py --gpu 1 --cn 4 --fr 32 --ks 5 --bn True --lr 1e-5 --step 100000 --bz 50 --train 65000 --val 400 --test 400 --noise 5
-# JOB: python AE.py --gpu 2 --cn 4 --fr 32 --ks 5 --bn True --lr 5e-6 --step 100000 --bz 50 --train 65000 --val 400 --test 400 --noise 5
-# JOB: python AE.py --gpu 3 --cn 4 --fr 32 --ks 5 --bn True --lr 1e-5 --step 100000 --bz 50 --train 65000 --val 400 --test 400 --noise 10
-
-# JOB: python AE.py --gpu 0 --cn 4 --fr 32 --ks 5 --bn True --lr 5e-6 --step 100000 --bz 50 --train 65000 --val 400 --test 400 --noise 10
-# JOB: python AE.py --gpu 1 --cn 4 --fr 32 --ks 5 --bn True --lr 1e-6 --step 100000 --bz 50 --train 65000 --val 400 --test 400 --noise 10
-# JOB: python AE.py --gpu 2 --cn 4 --fr 32 --ks 5 --bn True --lr 5e-6 --step 100000 --bz 50 --train 65000 --val 400 --test 400 --noise 15
-# JOB: python AE.py --gpu 3 --cn 4 --fr 32 --ks 5 --bn True --lr 1e-5 --step 100000 --bz 50 --train 65000 --val 400 --test 400 --noise 15
-
-# JOB: python AE.py --gpu 0 --cn 4 --fr 32 --ks 5 --bn True --lr 5e-6 --step 100000 --bz 50 --train 65000 --val 400 --test 400 --noise 20
-# JOB: python AE.py --gpu 1 --cn 4 --fr 32 --ks 5 --bn True --lr 1e-5 --step 100000 --bz 50 --train 65000 --val 400 --test 400 --noise 20
-# JOB: python AE.py --gpu 2 --cn 4 --fr 32 --ks 5 --bn True --lr 5e-6 --step 100000 --bz 50 --train 65000 --val 400 --test 400 --noise 25
-# JOB: python AE.py --gpu 3 --cn 4 --fr 32 --ks 5 --bn True --lr 1e-5 --step 100000 --bz 50 --train 65000 --val 400 --test 400 --noise 25
-
-# ### May 9, 2020
-# JOB: python AE.py --gpu 0 --cn 4 --fr 32 --ks 5 --bn True --lr 1e-4 --step 300000 --bz 50 --train 65000 --val 200 --test 200 --noise 40 --version 1
-# JOB: python AE.py --gpu 1 --cn 4 --fr 32 --ks 5 --bn True --lr 1e-4 --step 300000 --bz 50 --train 65000 --val 200 --test 200 --noise 50 --version 1
-
-# JOB: python AE.py --gpu 0 --cn 4 --fr 32 --ks 5 --bn True --lr 1e-5 --step 300000 --bz 50 --train 65000 --val 200 --test 200 --noise 40 --version 1
-# JOB: python AE.py --gpu 1 --cn 4 --fr 32 --ks 5 --bn True --lr 1e-5 --step 300000 --bz 50 --train 65000 --val 200 --test 200 --noise 50 --version 1
-
-JOB: python AE.py --gpu 0 --cn 6 --fr 32 --ks 5 --bn True --lr 1e-4 --step 300000 --bz 50 --train 65000 --val 200 --test 200 --noise 40 --version 1
-JOB: python AE.py --gpu 1 --cn 6 --fr 32 --ks 5 --bn True --lr 1e-4 --step 300000 --bz 50 --train 65000 --val 200 --test 200 --noise 50 --version 1
+## May 10, 2020
+JOB: python AE.py --gpu 0 --cn 4 --fr 32 --ks 3 --bn True --lr 1e-5 --step 300000 --bz 50 --train 33000 --val 200 --test 200 --version 2 --dataset scattered
+JOB: python AE.py --gpu 1 --cn 5 --fr 32 --ks 3 --bn True --lr 1e-5 --step 300000 --bz 50 --train 33000 --val 200 --test 200 --version 2 --dataset scattered
+JOB: python AE.py --gpu 2 --cn 6 --fr 32 --ks 3 --bn True --lr 1e-5 --step 300000 --bz 50 --train 33000 --val 200 --test 200 --version 2 --dataset scattered
+JOB: python AE.py --gpu 3 --cn 7 --fr 32 --ks 3 --bn True --lr 1e-5 --step 300000 --bz 50 --train 33000 --val 200 --test 200 --version 2 --dataset scattered
