25ac55f2c00cc3a8342bae2b048e2e9cc339bb58
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-05-11 12:47:53 -0500

    v100

--- FDA/gpu0.sh
+++ FDA/gpu0.sh
@@ -16,4 +16,4 @@
 # python AE.py --gpu 0 --cn 4 --fr 32 --ks 5 --bn True --lr 1e-4 --step 200000 --bz 50 --train 65000 --val 400 --test 400 --noise 10
 # python AE.py --gpu 0 --cn 4 --fr 32 --ks 3 --bn True --lr 1e-4 --step 200000 --bz 50 --train 65000 --val 200 --test 200 --noise 40 --version 2
 # python AE.py --gpu 0 --cn 4 --fr 32 --ks 3 --bn True --lr 1e-4 --step 200000 --bz 50 --train 65000 --val 200 --test 200 --noise 30 --version 2
-python AE_ssim.py --gpu 0 --cn 4 --fr 32 --ks 3 --bn True --lr 1e-5 --step 300000 --bz 50 --train 7100 --val 200 --test 200 --version 2 --dataset dense --loss ssim
+python AE_ssim.py --gpu 0 --cn 6 --fr 32 --ks 3 --bn True --lr 1e-3 --step 300000 --bz 50 --train 33000 --val 200 --test 200 --version 2 --dataset dense --loss ssim
--- FDA/gpu2.sh
+++ FDA/gpu2.sh
@@ -1,3 +1,3 @@
 # python AE.py --gpu 2 --cn 4 --fr 32 --ks 5 --bn True --lr 1e-4 --step 100000 --bz 50 --train 65000 --val 400 --test 400 --noise 5
 # python AE.py --gpu 2 --cn 4 --fr 32 --ks 3 --bn True --lr 1e-4 --step 200000 --bz 50 --train 65000 --val 200 --test 200 --noise 20 --version 2
-python AE_ssim.py --gpu 2 --cn 8 --fr 32 --ks 3 --bn True --lr 1e-3 --step 300000 --bz 50 --train 7100 --val 200 --test 200 --version 2 --dataset dense --loss ssim
+python AE_ssim.py --gpu 2 --cn 6 --fr 32 --ks 3 --bn True --lr 1e-3 --step 300000 --bz 50 --train 7100 --val 200 --test 200 --version 2 --dataset dense --loss ssim
