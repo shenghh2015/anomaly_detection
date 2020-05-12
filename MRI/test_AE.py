43eaf2fecf43a0fae3c447028a2ae6ed85985851
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-05-11 20:29:48 -0500

    v100

--- MRI/test_AE.py
+++ MRI/test_AE.py
@@ -176,29 +176,7 @@ def generate_folder(folder):
 	if not os.path.exists(folder):
 		os.system('mkdir -p {}'.format(folder))
 
-# def get_parameters(model_name):
-# 	splits = model_name.split('-')
-# 	for i in range(len(splits)):
-# 		if splits[i] == 'cn':
-# 			nb_cnn = int(splits[i+1])
-# 		elif splits[i] == 'fr':
-# 			filters = int(splits[i+1])
-# 		elif splits[i] == 'ks':
-# 			kernel_size = int(splits[i+1])
-# 		elif splits[i] == 'tr':
-# 			train = int(splits[i+1][:2])* 1000
-# 		elif splits[i] == 'vl':
-# 			val = int(splits[i+1])
-# 		elif splits[i] == 'test':
-# 			test = int(splits[i+1])
-# 		elif splits[i] == 'n':
-# 			noise = float(splits[i+1])
-# 	return nb_cnn, filters, kernel_size, train, val, test, noise
-
 gpu = 0; docker = True
-# noise = 0; version = 2
-# train, val, test = 65000, 200, 200
-
 os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu)
 
 if docker:
@@ -207,9 +185,9 @@ else:
 	output_folder = './data/MRI'
 
 #model_name = 'AE2-MRI-cn-4-fr-32-ks-3-bn-True-skp-False-res-False-lr-0.0001-stps-200000-bz-50-tr-65k-vl-200-test-200-n-0.0'
-#model_name = 'AE2-MRI-cn-4-fr-32-ks-3-bn-True-skp-False-res-False-lr-0.0001-stps-200000-bz-50-tr-65k-vl-200-test-200-n-40.0'
-# model_name = 'AE1-MRI-cn-4-fr-32-ks-5-bn-True-skp-False-res-False-lr-0.0001-stps-300000-bz-50-tr-65k-vl-200-test-200-n-50.0'
-model_name = 'AE2-MRI-cn-6-fr-32-ks-3-bn-True-skp-False-res-False-lr-0.0001-stps-200000-bz-50-tr-65k-vl-200-test-200-n-40.0-l-correntropy'
+# model_name = 'AE2-MRI-cn-4-fr-32-ks-3-bn-True-skp-False-res-False-lr-0.0001-stps-200000-bz-50-tr-65k-vl-200-test-200-n-40.0'
+model_name = 'AE1-MRI-cn-4-fr-32-ks-5-bn-True-skp-False-res-False-lr-0.0001-stps-300000-bz-50-tr-65k-vl-200-test-200-n-50.0'
+#model_name = 'AE2-MRI-cn-6-fr-32-ks-3-bn-True-skp-False-res-False-lr-0.0001-stps-200000-bz-50-tr-65k-vl-200-test-200-n-40.0-l-correntropy'
 splits = model_name.split('-')
 if len(splits[0])<=2:
 	version =1
@@ -233,22 +211,19 @@ for i in range(len(splits)):
 
 model_folder = os.path.join(output_folder, model_name)
 
+## load data
+print_red('Data loading ...')
 X_SA_trn, X_SA_val, X_SA_tst, X_SP_tst = load_MRI_true_data(docker = docker, train = train, val = val, normal = test, anomaly = test, noise = noise)
 X_SA_trn, X_SA_val, X_SA_tst, X_SP_tst = normalize_0_1(X_SA_trn), normalize_0_1(X_SA_val), normalize_0_1(X_SA_tst), normalize_0_1(X_SP_tst)
-# padding into 128x128 pixels
-# X_SA_trn, X_SA_val, X_SA_tst, X_SP_tst = pad_128(X_SA_trn), pad_128(X_SA_val), pad_128(X_SA_tst), pad_128(X_SP_tst)
-
 ## test data
 Xt = np.concatenate([X_SA_tst, X_SP_tst], axis = 0)
 yt = np.concatenate([np.zeros((len(X_SA_tst),1)), np.ones((len(X_SP_tst),1))], axis = 0).flatten()
-
 ## Dimension adjust
 X_SA_trn, X_SA_val, X_SA_tst, X_SP_tst, Xt = np.expand_dims(X_SA_trn, axis = 3), np.expand_dims(X_SA_val, axis = 3), np.expand_dims(X_SA_tst, axis = 3),\
 		 np.expand_dims(X_SP_tst, axis = 3), np.expand_dims(Xt, axis = 3)
+print_red('Data Loaded !')
 
-#nb_cnn = 4; 
 batch_norm = True 
-#filters = 32; kernel_size =3; 
 scope = 'base'
 x = tf.placeholder("float", shape=[None, 256, 256, 1])
 if version == 1:
@@ -256,8 +231,6 @@ if version == 1:
 elif version == 2:
 	h1, h2, y = auto_encoder(x, nb_cnn = nb_cnn, bn = batch_norm, filters = filters, kernel_size = [kernel_size, kernel_size], scope_name = scope)
 sqr_err = tf.square(y - x)
-ssim_err = 1- tf.image.ssim(y, x, max_val = 1.0)
-sigma = 0.1
 err_correntropy = -tf.exp(-tf.square(x - y)/sigma)
 
 # create a saver
@@ -272,74 +245,27 @@ saver = tf.train.Saver(key_direct, max_to_keep=1)
 for v in key_list:
 	print_green(v)
 
-continue_train = True
-separate = True
-# max_val, min_val = 100, 45
+# evaluate the model
 with tf.Session() as sess:
 	tf.global_variables_initializer().run(session=sess)
 	saver.restore(sess, model_folder+'/best')
-	#saver.restore(sess, model_folder+'/best-184100')  # noise 50
-	# saver.restore(sess, model_folder+'/best-170600')  # noise 40
-	# saver.restore(sess, model_folder+'/best-192600')  # noise 0
-	y_recon = y.eval(session = sess, feed_dict = {x:Xt})
-	recon_means = np.squeeze(np.apply_over_axes(np.mean, y_recon, axes = [1,2,3]))
-	recon_auc = roc_auc_score(yt, recon_means)
-	print_yellow('AUC: Recon Mean {0:.4f}'.format(recon_auc))
-	# separate calculation
-	if separate:
-		tst_errs = sqr_err.eval(session = sess, feed_dict = {x: X_SA_tst})
-		anomaly_errs = sqr_err.eval(session = sess, feed_dict = {x: X_SP_tst})
-# 		tst_errs = err_correntropy.eval(session = sess, feed_dict = {x: X_SA_tst})
-# 		anomaly_errs = err_correntropy.eval(session = sess, feed_dict = {x: X_SP_tst})
-		recon_errs = np.concatenate([tst_errs, anomaly_errs], axis = 0)
-		recon_errs = np.apply_over_axes(np.mean, recon_errs, [1,2,3]).flatten()
-		detect_auc = roc_auc_score(yt, recon_errs)
-		print_green('Anomaly detection auc: {0:.4f}'.format(detect_auc))
-		
-	ssim_errs = ssim_err.eval(session = sess, feed_dict = {x:Xt})
-	ssim_auc = roc_auc_score(yt, ssim_errs)
-	print_yellow('AUC: SSIM {0:.4f}'.format(ssim_auc))
-	tst_pixel_errs = sqr_err.eval(session = sess, feed_dict = {x:Xt})
-	tst_pixel_errs1 = []
-	max_val, min_val = np.max(tst_pixel_errs), np.min(tst_pixel_errs)
-	for i in range(tst_pixel_errs.shape[0]):
-		err = tst_pixel_errs[i,:]; err = (err -np.min(err))/(np.max(err)-np.min(err))*(max_val -min_val)+min_val 
-		tst_pixel_errs1.append(err.reshape(1,256,256,1))
-	tst_pixel_errs = np.concatenate(tst_pixel_errs1)
-	img_means = np.squeeze(np.apply_over_axes(np.mean, Xt, axes = [1,2,3]))
-	tst_img_errs = np.squeeze(np.apply_over_axes(np.mean, tst_pixel_errs, axes = [1,2,3]))
-	test_auc = roc_auc_score(yt, tst_img_errs)
-	mean_auc = roc_auc_score(yt, img_means)
-	print_block(symbol = '-', nb_sybl = 50)
-	print_yellow('AUC: AE {0:.4f} M: {1:.4f}'.format(test_auc, mean_auc))
+	# reconstructed images
+	Yn = y.eval(session = sess, feed_dict = {x: X_SA_tst}); Ya = y.eval(session = sess, feed_dict = {x: X_SP_tst})
+	y_recon = np.concatenate([Yn, Ya], axis = 0)
+	# reconstruction errors-based detection
+	norm_err_map = sqr_err.eval(session = sess, feed_dict = {x: X_SA_tst}); anomaly_err_map = sqr_err.eval(session = sess, feed_dict = {x: X_SP_tst})
+	recon_err_map = np.concatenate([norm_err_map, anomaly_err_map], axis = 0)
+	recon_errs = np.apply_over_axes(np.mean, recon_err_map, [1,2,3]).flatten(); AE_auc = roc_auc_score(yt, recon_errs)
+	# pixel mean-based detection
+	img_means = np.squeeze(np.apply_over_axes(np.mean, Xt, axes = [1,2,3])); MP_auc = roc_auc_score(yt, img_means)
+	print_yellow('AUC: AE {0:.4f} MP: {1:.4f}'.format(AE_auc, MP_auc))
 	print(model_name)
 	np.savetxt(os.path.join(model_folder,'AE_stat.txt'), tst_img_errs)
-	np.savetxt(os.path.join(model_folder,'Pixel_mean_stat.txt'), img_means)
-	np.savetxt(os.path.join(model_folder,'best_auc_n.txt'),[test_auc, mean_auc])
-	hist_file = os.path.join(model_folder,'hist_n-{}.png'.format(model_name))
-	plot_hist(hist_file, tst_img_errs[:int(len(tst_img_errs)/2)], tst_img_errs[int(len(tst_img_errs)/2):])
+	np.savetxt(os.path.join(model_folder,'MP_stat.txt'), img_means)
+	np.savetxt(os.path.join(model_folder,'best_auc.txt'),[AE_auc, MP_auc])
+	hist_file = os.path.join(model_folder,'hist-{}.png'.format(model_name))
+	plot_hist(hist_file, recon_errs[:int(len(recon_errs)/2)], recon_errs[int(len(recon_errs)/2):])
 	plot_hist_pixels(model_folder+'/hist_mean_pixel.png'.format(model_name), img_means[:int(len(img_means)/2)], img_means[int(len(img_means)/2):])
-	print_red('update best: {}'.format(model_name))
 	saver.save(sess, model_folder +'/best')
-	img_file_name = os.path.join(model_folder,'recon_n-{}.png'.format(model_name))
-	save_recon_images(img_file_name, Xt, y_recon, tst_pixel_errs, fig_size = [11,5])
-	
-	if continue_train:
-		recon_train = []
-		batch_size = 50
-		i = 0
-# 		while(batch_size*i < X_SA_trn.shape[0]):
-# 			if i % 100 == 0:
-# 				print('{}-th batch'.format(i))
-# 			batch_x = X_SA_trn[batch_size*i:min(batch_size*(i+1),X_SA_trn.shape[0])]
-# 			recon_train.append(y.eval(session = sess, feed_dict = {x:batch_x}))
-# 			i = i+1
-		recon_val = y.eval(session = sess, feed_dict = {x:X_SA_val})
-		recon_tst = y.eval(session = sess, feed_dict = {x:X_SA_tst})
-		recon_anomaly = y.eval(session = sess, feed_dict = {x: X_SP_tst})
-		generate_folder(model_folder + '/output_dataset')
-		np.save(model_folder + '/train.npy', recon_train_arr)
-		np.save(model_folder + '/val.npy', recon_val)
-		np.save(model_folder +'/test.npy', recon_tst)
-		np.save(model_folder + 'anomaly.npy', recon_anomaly)
+	save_recon_images(model_folder+'/recon-{}.png'.format(model_name), Xt, y_recon, recon_errs, fig_size = [11,5])
 	
