af4f9d2d0923f45c35a647b780228b0c136fe019
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-05-13 18:18:52 -0500

    v100

--- MRI/gen_artifacts/docker.sh
+++ MRI/gen_artifacts/docker.sh
@@ -1,2 +1,2 @@
 chcon -Rt svirt_sandbox_file_t /home/sh38/Anom_Detection/
-docker run --gpus all -v /home/sh38/Anom_Detection/:/data -w /data/anomaly_detection/MRI/ -it --user $(id -u):$(id -g) shenghh2020/tf_gpu_py3.5:latest
+docker run --gpus all -v /home/sh38/Anom_Detection/:/data -w /data/anomaly_detection/MRI/gen_artifacts -it --user $(id -u):$(id -g) shenghh2020/tf_gpu_py3.5:latest
