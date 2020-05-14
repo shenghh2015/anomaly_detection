f77785cc8c66543e054dd0626b0ff221c0b6e250
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-05-13 19:13:42 -0500

    v100

--- MRI/load_data.py
+++ MRI/load_data.py
@@ -29,6 +29,27 @@ from sklearn.metrics import roc_auc_score
 # 	X_trn, X_val, X_n, X_a = img[:train,:], img[65000:65000+val,:], img[65600:65600+normal,:], img_MP[65600:65600+anomaly,:]
 # 	return X_trn, X_val, X_n, X_a
 
+def load_MRI_data(docker = False, train = 65000, val = 600, normal = 1000, anomaly = 1000, noise_level = 0, us_factor = 4):
+	if docker:
+		dataset_folder = '/data/datasets/MRI'
+	else:
+		dataset_folder = '/shared/planck/CommonData/MRI/anomaly_detection_data'
+	if us_factor == 1 or us_factor ==2 or us_factor == 4:
+		us_factor = int(us_factor)
+	if noise_level == 0:
+		img = np.load(os.path.join(dataset_folder, 'axial_batch2_256x256.npy'))
+	else:
+		img = np.load(os.path.join(dataset_folder, 'axial_batch2_256x256_artifact-{}_noise-{}.npy'.format(1, noise_level)))
+	if noise_level == 0 and us_factor == 4:
+		img_MP =  np.load(os.path.join(dataset_folder, 'axial_batch2_256x256_artifact.npy'))
+	elif noise_level == 10 and us_factor == 4
+		img_MP =  np.load(os.path.join(dataset_folder, 'axial_batch2_256x256_artifact_noisy.npy'))
+	else:
+		img_MP =  np.load(os.path.join(dataset_folder, 'axial_batch2_256x256_artifact-{}_noise-{}.npy'.format(us_factor, noise_level)))
+
+	X_trn, X_val, X_n, X_a = img[:train,:], img[65000:65000+val,:], img[65600:65600+normal,:], img_MP[65600:65600+anomaly,:]
+	return X_trn, X_val, X_n, X_a
+
 def load_MRI_true_data(docker = False, train = 65000, val = 600, normal = 1000, anomaly = 1000, noise = 0):
 	if docker:
 		dataset_folder = '/data/datasets/MRI'
