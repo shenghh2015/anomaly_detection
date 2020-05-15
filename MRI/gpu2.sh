1017c7ebb7844990a4b9783d03f97b813693755a
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-05-15 03:54:29 -0500

    v100

--- MRI/gpu2.sh
+++ MRI/gpu2.sh
@@ -1,1 +1,1 @@
-python noiseAE.py --gpu 2 --cn 4 --fr 32 --ks 5 --bn True --lr 1e-4 --step 200000 --bz 50 --train 65000 --val 200 --test 200 --noise_level 0 --us_factor 2 --version 2
+python noiseAE.py --gpu 2 --cn 4 --fr 32 --ks 5 --bn True --lr 1e-4 --step 200000 --bz 50 --train 65000 --val 200 --test 200 --noise_level 15 --us_factor 4 --version 2
