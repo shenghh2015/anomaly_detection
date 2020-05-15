28242e601c33b45f16d657f9f70dd4b5ca512302
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-05-15 10:21:51 -0500

    v100

--- MRI/gpu1.sh
+++ MRI/gpu1.sh
@@ -4,4 +4,4 @@
 # python AE.py --gpu 1 --cn 4 --fr 32 --ks 5 --bn True --lr 1e-4 --step 200000 --bz 50 --train 65000 --val 200 --test 200 --noise 150 --version 2
 # python SAE.py --gpu 0 --cn1 4 --cn2 4 --fr 32 --ks 3 --bn True --lr 1e-4 --step 300000 --bz 50 --train 20000 --val 200 --test 200 --noise 80 --version 2 --loss1 correntropy --loss2 correntropy
 # python AE.py --gpu 1 --cn 4 --fr 32 --ks 5 --bn True --lr 1e-4 --step 200000 --bz 50 --train 65000 --val 200 --test 200 --noise 200 --version 2
-python noiseAE.py --gpu 1 --cn 4 --fr 32 --ks 5 --bn True --lr 1e-4 --step 200000 --bz 50 --train 65000 --val 200 --test 200 --noise_level 10 --us_factor 4 --version 2
+python noiseAE.py --gpu 1 --cn 4 --fr 32 --ks 5 --bn False --lr 1e-4 --step 200000 --bz 50 --train 65000 --val 200 --test 200 --noise_level 10 --us_factor 4 --version 2
