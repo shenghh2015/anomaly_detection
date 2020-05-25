e0dc61c15694dc3a91f6e51c427697a24142b558
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-05-25 18:00:00 -0500

    v100

--- MRI/multi_GPU.sh
+++ MRI/multi_GPU.sh
@@ -39,7 +39,7 @@
 # python noiseAE.py --gpu 0 --cn 4 --fr 32 --ks 5 --bn True --lr 1e-4 --step 200000 --bz 50 --train 65000 --val 200 --test 200 --noise_level 0 --us_factor 4 --version 2
 # JOB: python AE_train.py --gpu 1 --cn 5 --fr 32 --ks 5 --bn False --lr 1e-4 --step 100000 --bz 50 --version 4 --train 65000 --val 400 --test 1000 --loss 'mae'
 # JOB: python AE_train.py --gpu 0 --cn 5 --fr 32 --ks 5 --bn False --lr 1e-4 --step 100000 --bz 50 --version 4 --train 65000 --val 400 --test 1000 --loss 'mse'
-JOB: python AE_train.py --gpu 0 --cn 4 --fr 32 --ks 5 --bn False --lr 1e-4 --step 100000 --bz 50 --version 2 --train 65000 --val 400 --test 1000 --loss 'mae'
-JOB: python AE_train.py --gpu 1 --cn 4 --fr 32 --ks 5 --bn False --lr 1e-4 --step 100000 --bz 50 --version 3 --train 65000 --val 400 --test 1000 --loss 'mae'
+JOB: python AE_train.py --gpu 0 --cn 4 --fr 32 --ks 5 --bn True --lr 1e-4 --step 100000 --bz 50 --version 4 --train 65000 --val 400 --test 1000 --loss 'mae'
+JOB: python AE_train.py --gpu 1 --cn 4 --fr 32 --ks 5 --bn True --lr 1e-4 --step 100000 --bz 50 --version 4 --train 65000 --val 400 --test 1000 --loss 'mae'
 # JOB: python AE_train.py --gpu 1 --cn 6 --fr 32 --ks 5 --bn False --lr 1e-4 --step 100000 --bz 50 --version 4 --train 65000 --val 400 --test 1000 --loss 'mae'
 # JOB: python AE_train.py --gpu 0 --cn 4 --fr 32 --ks 5 --bn False --lr 1e-4 --step 100000 --bz 50 --version 4 --train 65000 --val 400 --test 1000 --loss 'mae'
