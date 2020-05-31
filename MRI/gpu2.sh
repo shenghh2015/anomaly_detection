b1216e1c6f4151e2616996290d526b0c5cfadf5e
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-05-31 09:02:49 -0500

    v100

--- MRI/gpu2.sh
+++ MRI/gpu2.sh
@@ -1,3 +1,3 @@
 # python noiseAE.py --gpu 2 --cn 4 --fr 32 --ks 5 --bn True --lr 1e-4 --step 200000 --bz 50 --train 65000 --val 200 --test 200 --noise_level 15 --us_factor 4 --version 
 # python AE_train.py --gpu 2 --cn 5 --fr 32 --ks 5 --bn False --lr 1e-4 --step 100000 --bz 50 --version 4 --train 65000 --val 400 --test 1000
-python AE_labels.py --gpu 2 --cn 4 --fr 32 --ks 5 --bn False --lr 1e-4 --step 200000 --bz 50 --version 3 --train 65000 --val 400 --test 1000 --loss mae
+python AE_labels.py --gpu 2 --cn 4 --fr 32 --ks 5 --bn False --lr 1e-4 --step 100000 --bz 50 --version 3 --train 65000 --val 400 --test 1000 --loss mae
