870fabdab3eaf201faee69fb2fb8f7ad6c82bd1e
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-06-01 23:36:36 -0500

    v100

--- MRI/gpu0.sh
+++ MRI/gpu0.sh
@@ -1,2 +1,3 @@
 # python AE_train.py --gpu 0 --cn 4 --fr 32 --ks 5 --bn False --lr 1e-4 --step 100000 --bz 50 --version 4 --train 65000 --val 400 --test 1000
+# python AE_labels.py --gpu 0 --cn 6 --fr 32 --ks 5 --bn True --lr 5e-6 --step 100000 --bz 50 --version 1 --train 65000 --val 400 --test 1000 --loss mae --ano_weight 0.01 --anomaly 4x
 python AE_labels.py --gpu 0 --cn 6 --fr 32 --ks 5 --bn True --lr 5e-6 --step 100000 --bz 50 --version 1 --train 65000 --val 400 --test 1000 --loss mae --ano_weight 0.01 --anomaly 4x
